aa252cdb39369a9d205a5fd34442afccd39c8d14
Omaribrahim43 <omar.migdadi43@gmail.com>
2023-10-07 21:34:13 +0300

    omar

--- src/Pages/login/login.js
+++ src/Pages/login/login.js
@@ -8,6 +8,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
+import { useNavigate } from "react-router";
+
 
 
 const Login = () => {
@@ -32,8 +34,10 @@ const Login = () => {
 
   const [data, setData] = useState([]);
 
+  let navigate = useNavigate();
   let currentPageUrl = window.location.href;
-
+  const movieToBook = sessionStorage.getItem('movieToBook');
+  const category = sessionStorage.getItem('category');
 
   useEffect(() => {
     // Check if the email matches the email regex
@@ -59,172 +63,177 @@ const Login = () => {
       .then(response => {
         const data = response.data;
         const user = data.find(item => item.email === email && item.pwd === pwd);
-  
+
         if (user) {
           console.log('Login successful');
-          
+
           // Store the user data as a string in sessionStorage
           sessionStorage.setItem('myData', JSON.stringify(user));
-          
+
           // Update the state to reflect the stored data
           setStoredData(user);
-          
+
           // Clear the input field
           setEmail('');
           setPwd('');
-          window.location.href = '/';
+          if (movieToBook) {
+            sessionStorage.removeItem('movieToBook');
+            navigate(`/allmovies/${category}/single/${movieToBook}`);
+          } else {
+            navigate(`/`);
+          }
 
         } else {
           console.log('Invalid email or password');
-          
+
         }
       })
       .catch(error => {
         console.error('Error:', error);
       });
   };
-  
+
 
   return (
     <div className="half container_register">
-      
+
       <div className="form_pading loginform ">
-                <h3 className="mb-5">
-                Login <strong>MovieStar</strong>
-                </h3>
-             
-                <Form onSubmit={handleLogin} className="Formlog">
-            
+        <h3 className="mb-5">
+          Login <strong>MovieStar</strong>
+        </h3>
 
-                  <div className="form-group first">
-                  <Form.Field>
-                    <label htmlFor="email">
-                      Email:
-                      <FontAwesomeIcon
-                        icon={faCheck}
-                        className={validEmail ? "valid" : "hide"}
-                      />
-                      <FontAwesomeIcon
-                        icon={faTimes}
-                        className={validEmail || !email ? "hide" : "invalid"}
-                      />
-                    </label>
+        <Form onSubmit={handleLogin} className="Formlog">
 
-                    <input
-                      type="email"
-                      className="form-control"
-                      placeholder="your-email@gmail.com"
-                      id="email"
-                      onChange={(e) => setEmail(e.target.value)}
-                      value={email}
-                      required
-                      aria-invalid={validEmail ? "false" : "true"}
-                      aria-describedby="emailnote"
-                      onFocus={() => setEmailFocus(true)}
-                      onBlur={() => setEmailFocus(false)}
-                    />
-                    </Form.Field>
 
-                    <p
-                      id="emailnote"
-                      className={
-                        emailFocus && email && !validEmail
-                          ? "instructions"
-                          : "offscreen"
-                      }
-                    >
-                      <FontAwesomeIcon icon={faInfoCircle} />
-                      Valid email address format required.
-                    </p>
-                  </div>
+          <div className="form-group first">
+            <Form.Field>
+              <label htmlFor="email">
+                Email:
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className={validEmail ? "valid" : "hide"}
+                />
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className={validEmail || !email ? "hide" : "invalid"}
+                />
+              </label>
 
-                  <div className="form-group last mb-3">
-                  <Form.Field>
-                    <label htmlFor="password">
-                      Password:
-                      <FontAwesomeIcon
-                        icon={faCheck}
-                        className={validPwd ? "valid" : "hide"}
-                      />
-                      <FontAwesomeIcon
-                        icon={faTimes}
-                        className={validPwd || !pwd ? "hide" : "invalid"}
-                      />
-                    </label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="your-email@gmail.com"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+                aria-invalid={validEmail ? "false" : "true"}
+                aria-describedby="emailnote"
+                onFocus={() => setEmailFocus(true)}
+                onBlur={() => setEmailFocus(false)}
+              />
+            </Form.Field>
 
-                    <input
-                      type="password"
-                      className="form-control"
-                      placeholder="Your Password"
-                      id="password"
-                      onChange={(e) => setPwd(e.target.value)}
-                      value={pwd}
-                      required
-                      aria-invalid={validPwd ? "false" : "true"}
-                      aria-describedby="pwdnote"
-                      onFocus={() => setPwdFocus(true)}
-                      onBlur={() => setPwdFocus(false)}
-                    />
-                    </Form.Field>
-                    <p
-                      id="pwdnote"
-                      className={
-                        pwdFocus && pwd && !validPwd
-                          ? "instructions"
-                          : "offscreen"
-                      }
-                    >
-                      <FontAwesomeIcon icon={faInfoCircle} />
-                      8 to 24 characters.
-                      <br />
-                      Must include uppercase and lowercase letters, a number and a
-                      special character.
-                      <br />
-                      Allowed special characters:{" "}
-                      <span aria-label="exclamation mark">!</span>{" "}
-                      <span aria-label="at symbol">@</span>{" "}
-                      <span aria-label="hashtag">#</span>{" "}
-                      <span aria-label="dollar sign">$</span>{" "}
-                      <span aria-label="percent">%</span>
-                    </p>
-                  </div>
-                 
+            <p
+              id="emailnote"
+              className={
+                emailFocus && email && !validEmail
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+              Valid email address format required.
+            </p>
+          </div>
 
-         
-                  <div className="d-flex mb-10 align-items-center">
-                    <span className="ml-auto">
-                      <a href="#" className="forgot-pass">
-                        Forget your password ?
-                      </a>
-                    </span>
-                  </div>
-                
-                  <Button
-                    type="submit"
-                    className="signup"
-                    disabled={
-                      !validPwd ||
-                     
-                      !validEmail
-                    }
-                  >
-                   Log in
-                  </Button>
-                </Form>
-              </div>
-            
+          <div className="form-group last mb-3">
+            <Form.Field>
+              <label htmlFor="password">
+                Password:
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className={validPwd ? "valid" : "hide"}
+                />
+                <FontAwesomeIcon
+                  icon={faTimes}
+                  className={validPwd || !pwd ? "hide" : "invalid"}
+                />
+              </label>
 
-        <div
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Your Password"
+                id="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={pwd}
+                required
+                aria-invalid={validPwd ? "false" : "true"}
+                aria-describedby="pwdnote"
+                onFocus={() => setPwdFocus(true)}
+                onBlur={() => setPwdFocus(false)}
+              />
+            </Form.Field>
+            <p
+              id="pwdnote"
+              className={
+                pwdFocus && pwd && !validPwd
+                  ? "instructions"
+                  : "offscreen"
+              }
+            >
+              <FontAwesomeIcon icon={faInfoCircle} />
+              8 to 24 characters.
+              <br />
+              Must include uppercase and lowercase letters, a number and a
+              special character.
+              <br />
+              Allowed special characters:{" "}
+              <span aria-label="exclamation mark">!</span>{" "}
+              <span aria-label="at symbol">@</span>{" "}
+              <span aria-label="hashtag">#</span>{" "}
+              <span aria-label="dollar sign">$</span>{" "}
+              <span aria-label="percent">%</span>
+            </p>
+          </div>
+
+
+
+          <div className="d-flex mb-10 align-items-center">
+            <span className="ml-auto">
+              <a href="#" className="forgot-pass">
+                Forget your password ?
+              </a>
+            </span>
+          </div>
+
+          <Button
+            type="submit"
+            className="signup"
+            disabled={
+              !validPwd ||
+
+              !validEmail
+            }
+          >
+            Log in
+          </Button>
+        </Form>
+      </div>
+
+
+      <div
         className="logbg image">
-        
-        <img src="close-up-popcorn-near-clapperboard-film-bobbin.jpg"alt="Cinema"></img> 
-        </div>
-      
+
+        <img src="close-up-popcorn-near-clapperboard-film-bobbin.jpg" alt="Cinema"></img>
+      </div>
+
       {/* <div className="col-md-6">
         <img className="signupimg " src="cinema.jpg" alt="Cinema" />
       </div> */}
-      </div>
-    
+    </div>
+
   );
 };
 
